e02b516afb6b78f2809bd688e8517fe482fbb4ab
Kimminsu <alstn2468_@naver.com>
2020-06-04 14:57:30 +0900

    [#5] Fix day 4 challenge code

--- DAY_4_CHALLENGE/src/index.js
+++ DAY_4_CHALLENGE/src/index.js
@@ -4,20 +4,24 @@ import "./styles.css";
 
 const colors = ["#E1AD01", "#B574EC", "#2A9DF4"];
 
-const backgroundCallBack = event => {
-  const { innerWidth, screenX } = event.target;
+const setBackgroundColor = (innerWidth, screenX) => {
+    if (screenX * 0.5 >= innerWidth) {
+        body.style.background = colors[2];
+    } else if (screenX * 0.7 >= innerWidth) {
+        body.style.background = colors[1];
+    } else {
+        body.style.background = colors[0];
+    }
+};
 
-  if (screenX * 0.5 >= innerWidth) {
-    body.style.background = colors[2];
-  } else if (screenX * 0.7 >= innerWidth) {
-    body.style.background = colors[1];
-  } else {
-    body.style.background = colors[0];
-  }
+const backgroundCallBack = (event) => {
+    const { innerWidth, screenX } = event.target;
+
+    setBackgroundColor(innerWidth, screenX);
 };
 
 const body = document.querySelector("body");
-body.style.background = colors[0];
+setBackgroundColor(window.innerWidth, window.screenX);
 
 const hello = document.createElement("h1");
 hello.innerHTML = "Hello!";
@@ -25,5 +29,4 @@ hello.style.color = "#FFFFFF";
 
 body.appendChild(hello);
 
-window.addEventListener("load", backgroundCallBack);
 window.addEventListener("resize", backgroundCallBack);
