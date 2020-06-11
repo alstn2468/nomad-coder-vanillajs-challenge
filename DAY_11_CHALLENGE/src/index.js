0cf81c54934ca4bd6903bcfcce60d7cd878b6118
Kimminsu <alstn2468_@naver.com>
2020-06-11 11:52:57 +0900

    [#30] Add operator button callback and fix number button callback

--- DAY_11_CHALLENGE/src/index.js
+++ DAY_11_CHALLENGE/src/index.js
@@ -7,8 +7,22 @@ const resultNumber = document.querySelector(".result-number");
 const acButton = document.querySelector("#ac");
 const pmButton = document.querySelector("#pm");
 const commaButton = document.querySelector("#com");
+const modButton = document.querySelector("#mod");
+const divButton = document.querySelector("#div");
+const mulButton = document.querySelector("#mul");
+const addButton = document.querySelector("#add");
+const subButton = document.querySelector("#sub");
+const equButton = document.querySelector("#equ");
+const operators = {
+    mod: (a, b) => Number(a) % Number(b),
+    div: (a, b) => Number(a) / Number(b),
+    mul: (a, b) => Number(a) * Number(b),
+    sub: (a, b) => Number(a) - Number(b),
+    add: (a, b) => Number(a) + Number(b),
+};
 
 let selectedOperator = null;
+let beforeNumber = null;
 
 function onClickCommaButton() {
     let tempNumber = resultNumber.innerText;
@@ -17,19 +31,25 @@ function onClickCommaButton() {
         return;
     }
 
-    resultNumber.innerText += ".0";
+    resultNumber.innerText += ".";
 }
 
-function onClickNumberButton(num) {
-    let tempNumber = resultNumber.innerText;
+function onClickNumberButton(event) {
+    if (event.target.classList.value.includes("number-button")) {
+        let tempNumber = resultNumber.innerText;
+        let newNumber = event.target.innerText;
 
-    if (tempNumber === "0") {
-        tempNumber = num;
-    } else {
-        tempNumber += num;
+        if (selectedOperator && !beforeNumber) {
+            beforeNumber = tempNumber;
+            tempNumber = newNumber;
+        } else if (tempNumber === "0") {
+            tempNumber = newNumber;
+        } else {
+            tempNumber += newNumber;
+        }
+
+        resultNumber.innerText = tempNumber;
     }
-
-    resultNumber.innerText = tempNumber;
 }
 
 function onClickResetNumberButton() {
@@ -55,10 +75,35 @@ function onClickChangeSignButton() {
 }
 
 function initNumberButtons() {
-    for (let i = 0; i < 10; i++) {
-        const button = document.querySelector(`#number-${i}`);
-        button.addEventListener("click", () => onClickNumberButton(i));
+    const calculateWrapper = document.querySelector(".calculate-pad-wrapper");
+
+    calculateWrapper.addEventListener("click", onClickNumberButton);
+}
+
+function onClickOperatorButton(event) {
+    let tempNumber = resultNumber.innerText;
+
+    if (selectedOperator) {
+        resultNumber.innerText = operators[selectedOperator](
+            beforeNumber,
+            tempNumber
+        );
+        beforeNumber = null;
     }
+
+    selectedOperator = event.target.id;
+}
+
+function onClickEqualButton() {
+    const afterNumber = resultNumber.innerText;
+
+    resultNumber.innerText = operators[selectedOperator](
+        beforeNumber,
+        afterNumber
+    );
+
+    beforeNumber = null;
+    selectedOperator = null;
 }
 
 function init() {
@@ -66,6 +111,12 @@ function init() {
     acButton.addEventListener("click", onClickResetNumberButton);
     pmButton.addEventListener("click", onClickChangeSignButton);
     commaButton.addEventListener("click", onClickCommaButton);
+    modButton.addEventListener("click", onClickOperatorButton);
+    divButton.addEventListener("click", onClickOperatorButton);
+    mulButton.addEventListener("click", onClickOperatorButton);
+    addButton.addEventListener("click", onClickOperatorButton);
+    subButton.addEventListener("click", onClickOperatorButton);
+    equButton.addEventListener("click", onClickEqualButton);
 }
 
 init();
