19bfb0de2f9fe38bb0baf57f050629a33312313c
alstn2468 <alstn2468_@naver.com>
2020-06-13 11:11:49 +0900

    [#31] Add day 13-14 challenge code

---
+++ DAY_13_CHALLENGE/js/weather.js
@@ -0,0 +1,57 @@
+const API_KEY = 'secret';
+const COORDS_LS = 'coords';
+const weather = document.querySelector('.js-weather');
+
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            const temperature = json.main.temp;
+            const location = json.name;
+            weather.innerText = `${Math.floor(
+                ((temperature - 32) * 5) / 9
+            )}°C, ${location}`;
+        });
+}
+
+function saveLocation(location) {
+    localStorage.setItem(COORDS_LS, JSON.stringify(location));
+}
+
+function handleGeoError() {
+    console.error('Fail to load location');
+}
+
+function handleGeoSuccess(pos) {
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+    const location = {
+        latitude,
+        longitude,
+    };
+    saveLocation(location);
+    getWeather(location.latitude, location.longitude);
+}
+
+function getCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS_LS);
+    if (loadedCoords === null) {
+        getCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+init();
